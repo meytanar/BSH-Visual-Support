a88226df660c5ed7a84323f6d8a7dccbd43a80b4
BSH_MRM <bsh_mrm@BSH-MRM-MacBook-Pro.local>
2018-11-10 17:00:45 +0300

    Initial Commit

---
+++ BSH Visual Support/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BSH Visual Support
+//
+//  Created by BSH_MRM on 10.11.2018.
+//  Copyright © 2018 BSH_MRM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
